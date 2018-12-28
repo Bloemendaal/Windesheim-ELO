1bbbe3a570b857da7196d3c22fcb7578a30c4a24
Bloemendaal <casperbloemendaal@gmail.com>
2018-12-29 00:21:12 +0100

    Create ELO.js

---
+++ ELO.js
@@ -0,0 +1,84 @@
+// ==UserScript==
+// @name         Windesheim Elo Fix
+// @namespace    http://casperbloemendaal.nl/
+// @version      0.1
+// @description  Make the Elo great (again)!
+// @author       Casper Bloemendaal
+// @match        https://elo.windesheim.nl/Start.aspx
+// @grant        none
+// @run-at       document-end
+// @noframes
+// ==/UserScript==
+
+(function() {
+   'use strict';
+
+   document.getElementById('tns').addEventListener("load", function(){
+      function continueB(el) {
+         try {
+            var c = el.contentWindow.document.getElementsByClassName('WidgetIframe');
+         } catch(err) {
+            c = [];
+         }
+
+         if (c.length) {
+            continueC(c);
+         } else {
+            el.addEventListener("load", function(){
+               c = this.contentWindow.document.getElementsByClassName('WidgetIframe');
+               if (c) {
+                  continueC(c);
+               }
+            });
+         }
+      }
+
+      function continueC(el) {
+         Array.prototype.forEach.call(el, d => {
+            d.addEventListener("load", function(){
+               this.style.height = this.contentWindow.document.body.scrollHeight + 'px';
+            });
+            d.style.height = d.contentWindow.document.body.scrollHeight + 'px';
+         });
+      }
+
+      var observer = new MutationObserver(function(mutations) {
+         document.getElementById('tns').contentWindow.document.querySelectorAll('.StudyRoutePageViewer > iframe').forEach(function(a){
+            try {
+               var b = this.contentWindow.document.getElementById('widgetsiframe');
+            } catch(err) {
+               b = null;
+            }
+            if (b) {
+               continueB(b);
+            } else {
+               a.addEventListener("load", function() {
+                  var b = this.contentWindow.document.getElementById('widgetsiframe');
+                  if (b) {
+                     continueB(b);
+                  }
+               });
+            }
+
+            try {
+               a.contentWindow.document.getElementsByClassName('detailintro')[0].remove();
+            } catch(err) {
+               a.addEventListener("load", function() {
+                  var r = this.contentWindow.document.getElementsByClassName('detailintro')[0];
+                  if (r) {
+                     r.remove();
+                  }
+               });
+            }
+
+         });
+      });
+
+      observer.observe(document.getElementById('tns').contentWindow.document.body, {
+         attributes: true,
+         childList: true,
+         characterData: true
+      });
+   });
+
+})();
