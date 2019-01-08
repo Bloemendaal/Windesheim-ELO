0f1ba11e7763939f6faff43745782cdebfd99e12
Bloemendaal <casperbloemendaal@gmail.com>
2019-01-08 20:12:46 +0100

    Update MDELO.user.js

--- MDELO.user.js
+++ MDELO.user.js
@@ -8,7 +8,7 @@
 // @downloadURL   https://github.com/Bloemendaal/Windesheim-ELO/raw/master/MDELO.user.js
 // @updateURL     https://github.com/Bloemendaal/Windesheim-ELO/raw/master/MDELO.user.js
 // @supportURL    https://github.com/Bloemendaal/Windesheim-ELO/issues
-// @version       1.5
+// @version       1.5.1
 
 // @match         https://elo.windesheim.nl/*
 // @grant         none
@@ -22,7 +22,7 @@
 (function() {
    'use strict';
 
-   var version = 1.5;
+   var version = 1.51;
    var tab     = false;
    var hidenav = false;
    var lang    = 0;
