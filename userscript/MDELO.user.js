f9b38438ee21d10c72667bb3164310808e0c40cc
Bloemendaal <casperbloemendaal@gmail.com>
2019-01-19 13:03:35 +0100

    v1.5.4

--- userscript/MDELO.user.js
+++ userscript/MDELO.user.js
@@ -8,7 +8,7 @@
 // @downloadURL   https://github.com/Bloemendaal/Windesheim-ELO/raw/v1.5.3/userscript/MDELO.user.js
 // @updateURL     https://github.com/Bloemendaal/Windesheim-ELO/raw/v1.5.3/userscript/MDELO.user.js
 // @supportURL    https://github.com/Bloemendaal/Windesheim-ELO/issues
-// @version       1.5.3
+// @version       1.5.4
 
 // @match         https://elo.windesheim.nl/*
 // @grant         none
@@ -22,7 +22,7 @@
 (function() {
    'use strict';
 
-   var version = 1.53;
+   var version = 1.54;
    var tab     = false;
    var hidenav = false;
    var lang    = 0;
@@ -121,6 +121,59 @@
             container: 'folder'
          },
          functions: {
+            fab: function(){
+               $.ajax({
+                  url: '/Services/CMS.asmx/ExportFolder',
+                  type: 'GET',
+                  data: {
+                     folderId: path[path.length - 1],
+                     download: null
+                  },
+                  cache:false,
+                  xhrFields:{
+                      responseType: 'blob'
+                  },
+                  success: function(response, status, xhr) {
+                     // check for a filename
+                     var filename = "";
+                     var disposition = xhr.getResponseHeader('Content-Disposition');
+                     if (disposition && disposition.indexOf('attachment') !== -1) {
+                        var filenameRegex = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/;
+                        var matches = filenameRegex.exec(disposition);
+                        if (matches != null && matches[1]) filename = matches[1].replace(/['"]/g, '');
+                     }
+
+                     var type = xhr.getResponseHeader('Content-Type');
+                     var blob = new Blob([response], { type: type });
+
+                     if (typeof window.navigator.msSaveBlob !== 'undefined') {
+                        // IE workaround for "HTML7007: One or more blob URLs were revoked by closing the blob for which they were created. These URLs will no longer resolve as the data backing the URL has been freed."
+                        window.navigator.msSaveBlob(blob, filename);
+                     } else {
+                        var URL = window.URL || window.webkitURL;
+                        var downloadUrl = URL.createObjectURL(blob);
+
+                        if (filename) {
+                           // use HTML5 a[download] attribute to specify filename
+                           var a = document.createElement("a");
+                           // safari doesn't support this yet
+                           if (typeof a.download === 'undefined') {
+                              window.location = downloadUrl;
+                           } else {
+                              a.href = downloadUrl;
+                              a.download = filename;
+                              document.body.appendChild(a);
+                              a.click();
+                           }
+                        } else {
+                           window.location = downloadUrl;
+                        }
+
+                        setTimeout(function () { URL.revokeObjectURL(downloadUrl); }, 100); // cleanup
+                     }
+                  }
+               });
+            },
             navback: function(){
                $('#nav-menu-list > li').removeClass('mdc-list-item--activated');
                $('#nav-menu-list > li[data-id="2"]').addClass('mdc-list-item--activated');
@@ -435,8 +488,14 @@
 
       }
 
-      $('#container > *, #nav > *:not(#nav-focus)').hide();
-      $('#container-' + display.container + ', #nav-' + display.nav).show();
+      if (display.container) {
+         $('#container > *').hide();
+         $('#container-' + display.container).show();
+      }
+      if (display.nav) {
+         $('#nav > *:not(#nav-focus)').hide();
+         $('#nav-' + display.nav).show();
+      }
 
       if (t) {
          $('#title').html(printLanguages(t));
@@ -625,7 +684,10 @@
                      nl: 'Map omhoog',
                      de: '&Uuml;bergeordneter Ordner'
                   }) + '</span></li><hr class="mdc-list-divider">');
-               }
+                  if (pages[tab].name == 'portfolio') {
+                     setPage({fab: 'save_alt'});
+                  } else { setPage({fab: false}); }
+               } else { setPage({fab: false}); }
             }
          } else {
             $('#nav-folder-list li.mdc-list-item').removeClass('mdc-list-item--activated');
